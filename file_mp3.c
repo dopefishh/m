124d04a59639df1976b37b2d61042670a72e72a3
Mart Lubbers <mart@martlubbers.net>
2017-07-21 11:07:22 +0200

    tag adding works, only a mapping now

--- file_mp3.c
+++ file_mp3.c
@@ -7,56 +7,110 @@
 #include "db.h"
 #include "log.h"
 
-void process_tag(unsigned int nfields, union id3_field *f, char **key, char **value)
+uint32_t process_ntags(struct id3_tag *t)
 {
-	char buf[25];
-	for(unsigned int i = 0; i<nfields; i++){
-		switch(id3_field_type(&f[i])){
+	uint32_t count = 0;
+	for(unsigned int i = 0; i<t->nframes; i++){
+		struct id3_frame *frame = t->frames[i];
+		for(unsigned int j = 0; j<frame->nfields; j++){
+			union id3_field field = frame->fields[j];
+			switch(field.type){
+			//Not parsed
+			case ID3_FIELD_TYPE_BINARYDATA:
+			case ID3_FIELD_TYPE_FRAMEID:
+			case ID3_FIELD_TYPE_TEXTENCODING:
+				break;
+			//Possibly multiple items
+			case ID3_FIELD_TYPE_LATIN1LIST:
+				count += field.latin1list.nstrings;
+				break;
+			case ID3_FIELD_TYPE_STRINGLIST:
+				count += field.stringlist.nstrings;
+				break;
+			//All others are single items
+			default:
+				count++;
+				break;
+			}
+		}
+	}
+	return count;
+}
+
+void process_frame(struct id3_frame *fr, char **ks, char **vs, uint32_t *ti)
+{
+	uint32_t oldti = *ti;
+
+	//If it is a user tag, we assume the first value to be the tagname
+	unsigned int st = strcmp(fr->id, "TXXX") == 0 ? 1 : 0;
+	char *key = NULL;
+
+	//Extract fields
+	for(unsigned int i = 0; i<fr->nfields; i++){
+		union id3_field f = fr->fields[i];
+		switch(f.type){
+		//Skip
+		case ID3_FIELD_TYPE_BINARYDATA:
 		case ID3_FIELD_TYPE_TEXTENCODING:
-			logmsg(debug, "Textencoding\n");
+		case ID3_FIELD_TYPE_FRAMEID:
 			break;
 		case ID3_FIELD_TYPE_LATIN1:
-			logmsg(debug, "Latin1\n");
-			break;
 		case ID3_FIELD_TYPE_LATIN1FULL:
-			logmsg(debug, "Latin1full\n");
+			if(st == 1 && key == NULL)
+				key = safe_strdup((char *)f.latin1.ptr);
+			else
+				vs[(*ti)++] = safe_strdup((char *)f.latin1.ptr);
 			break;
 		case ID3_FIELD_TYPE_LATIN1LIST:
-			logmsg(debug, "Latin1list\n");
+			if(st == 1)
+				key = safe_strdup(
+					(char *)f.latin1list.strings[0]);
+			for(unsigned int j = 0; j<f.latin1list.nstrings; j++)
+				vs[(*ti)++] = safe_strdup(
+					(char *)f.latin1list.strings[j]);
 			break;
 		case ID3_FIELD_TYPE_STRING:
-			logmsg(debug, "string\n");
-			break;
 		case ID3_FIELD_TYPE_STRINGFULL:
-			logmsg(debug, "stringfull\n");
+			if(st == 1 && key == NULL)
+				key = (char *)id3_ucs4_utf8duplicate(
+					f.string.ptr);
+			else
+				vs[(*ti)++] = (char *)id3_ucs4_utf8duplicate(
+					f.string.ptr);
 			break;
 		case ID3_FIELD_TYPE_STRINGLIST:
-			logmsg(debug, "stringlist\n");
+			if(st == 1)
+				key = (char *)id3_ucs4_utf8duplicate(
+					f.stringlist.strings[0]);
+			for(unsigned int j = 0; j<f.stringlist.nstrings; j++)
+				vs[(*ti)++] = (char *)id3_ucs4_utf8duplicate(
+					f.stringlist.strings[j]);
 			break;
 		case ID3_FIELD_TYPE_LANGUAGE:
-			logmsg(debug, "language\n");
-			break;
-		case ID3_FIELD_TYPE_FRAMEID:
-			logmsg(debug, "frameid\n");
-			break;
 		case ID3_FIELD_TYPE_DATE:
-			logmsg(debug, "date\n");
+			vs[(*ti)++] = safe_strdup(f.immediate.value);
 			break;
 		case ID3_FIELD_TYPE_INT8:
 		case ID3_FIELD_TYPE_INT16:
 		case ID3_FIELD_TYPE_INT24:
 		case ID3_FIELD_TYPE_INT32:
 		case ID3_FIELD_TYPE_INT32PLUS:
-			sprintf(buf, "%ld", f[i].number.value);
-			*value = safe_strdup(buf);
-			break;
-		case ID3_FIELD_TYPE_BINARYDATA:
-			logmsg(debug, "Binary tags not supported, images?\n");
-//			*value = safe_strdup(id3_field_getbinarydata(
-//				&f[i], NULL);
+			vs[*ti] = safe_malloc(25);
+			sprintf(vs[(*ti)++], "%ld", f.number.value);
 			break;
 		}
 	}
+
+	//Determine frame type
+	//If not a user tag, we look it up
+	if(key == NULL){
+		//Lookup key
+		key = safe_strdup("lookup");
+	}
+	for(uint32_t i = oldti; i<*ti; i++){
+		ks[i] = safe_strdup(key);
+	}
+	free(key);
 }
 
 bool process_mp3(char *p, struct db_file *f)
@@ -66,13 +120,19 @@ bool process_mp3(char *p, struct db_file *f)
 		return false;
 
 	struct id3_tag *mt = id3_file_tag(mf);
-	file_tag_init(f, mt->nframes);
+	logmsg(info, "Found %lu tags\n", process_ntags(mt));
+	file_tag_init(f, process_ntags(mt));
 
+	uint32_t tagindex = 0;
 	for(unsigned int i = 0; i<mt->nframes; i++){
-		struct id3_frame *fr = mt->frames[i];
-		logmsg(debug, "frame %d: %s\n", i, fr->description);
-		process_tag(fr->nfields, fr->fields,
-			&f->tags->keys[i], &f->tags->values[i]);
+		process_frame(mt->frames[i],
+			f->tags->values, f->tags->keys, &tagindex);
 	}
+	logmsg(info, "Really only written up to %d tags\n", tagindex-1);
+	f->tags->ntags = tagindex;
+	f->tags->keys = safe_realloc(f->tags->keys,
+		f->tags->ntags*sizeof(char *));
+	f->tags->values = safe_realloc(f->tags->values,
+		f->tags->ntags*sizeof(char *));
 	return id3_file_close(mf) == 0;
 }
